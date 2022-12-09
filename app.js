b1d3be1b8d66a201da0000ecb41b6c983b167c0a
rahmanaga <78140740+rahmanaga@users.noreply.github.com>
2022-12-09 15:39:58 +0400

    Add some pseudocode for game

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
+//write a function named getComputerChoice that randomly choose among three choices
 
+//create variable computerChoice
+
+//Create a list from three choices and store it in new variable named choices
+
+// Generate a random number between zero and two(inclusively) and store it in randomNum variable
+
+//Use randomNum to choose randomly among choices using bracket notation and store the value in computerChoice
+
+//Return computerChoice from function
+
+//Write function named findWinner that takes two parameters(playerChoice , computerChoice)
+// and checks if there is a winner or it is a tie
+
+//If playerChoice is equal to computerChoice then it is a tie and return 1
+
+//Else if playerChoice is equal to rock then if computerChoice is to equal to the scissors then
+//"Player won the round" and return 0
+
+////Else if computerChoice is equal to paper then "Computer won this round" and return 2
+
+//Else if playerChoice is equal to scissors then if computerChoice is to equal to the paper then
+// "Player won the round" and return 0
+
+////Else if computerChoice is equal to rock then "Computer won this round" and return 2
+
+//Else if playerChoice is equal to else(paper) then if computerChoice is to equal to the rock then
+// "Player won the round" and return 0
+
+////Else if computerChoice is equal to scissors then "Computer won this round" and return 2
+
+//Create function named playGame
+
+//Create playerScore and computerScore variables and initialize them with value of 0
+
+// create variable computerChoice
+
+//create variable playerChoice
+
+//Write for loop to play five round of game
+
+//Call getComputerChoice function and store the value in computerChoice variable
+
+//Ask what they want to choose among three choices and store it in playerChoice variable
+
+// Call findWinner function with two parameters(playerChoice,computerChoice)
+//and if returned value is equal to 0 then add one to playerScore ,else if value is equal to the 2 then
+// add one to the computerScore
+
+//After loop finished check if playerScore is greater than computerScore then display
+// "Player won the game" else if compChoice is greater then display "Computer won the game"
+//else display "DRAW!"
+
+//Call the playGame function
